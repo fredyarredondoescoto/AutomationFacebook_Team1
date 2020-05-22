6812cdd496b898d700361eb0adc7e6103957a3fe
JoseArielMartinezCorona <61592293+JoseArielMartinezCorona@users.noreply.github.com>
2020-05-22 08:55:59 -0500

    Proyecto Base
    Se agregaron los archivos base para la practica de automatización.

---
+++ AutomationPractice/AutomationPractice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AutomationPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
