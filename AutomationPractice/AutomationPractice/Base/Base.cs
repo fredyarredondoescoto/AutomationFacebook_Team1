bc0b920fad168422c547a27d298ca01fa5b62b37
Jmartinez <juanmanuelmartinezlucio@gmail.com>
2020-05-22 09:19:09 -0500

    Carpetas
    Se agregaron carpetas y base de la practica

---
+++ AutomationPractice/AutomationPractice/Base/Base.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutomationPractice.Base
+{
+    class Base
+    {
+        public RemoteWebDriver SetUp()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("--disable-notifications");
+            RemoteWebDriver driver = new ChromeDriver(options);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://www.facebook.com/");
+            return driver;
+        }
+    }
+}
