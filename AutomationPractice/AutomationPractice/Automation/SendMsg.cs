8cd4465892c8a2f52049516b4cb6a94d8b0dca4c
Alfredo Arredondo <fredyarredondoescoto@gmail.com>
2020-05-29 08:58:13 -0500

    Buscar contacto y enviar msj
    Se agrego la clase para la automatización de buscar un contacto en la lista de chat y enviar un mensaje

---
+++ AutomationPractice/AutomationPractice/Automation/SendMsg.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
+using AutomationPractice.Locators;
+
+namespace AutomationPractice.Automation
+{
+    class SendMsg
+    {
+        private RemoteWebDriver driver;
+        private WebDriverWait wait;
+        public WallLocators wallPage;
+
+        public SendMsg(RemoteWebDriver Driver, WebDriverWait Wait)
+        {
+            driver = Driver;
+            wait = Wait;
+        }
+
+        public bool SentTheMessage(RemoteWebDriver driver)
+        {
+            var isMessageSend = false;
+            try
+            {
+                wallPage = new WallLocators(driver,wait);
+
+                //CLIC PARA ABRIR LA LISTA DE CHAT
+                wallPage.FBChatList.Click();
+                //ENVIAR TEXTO A LA BUSQUEDA
+                wallPage.FBClSearchContact.SendKeys("Oscar Cova");
+                //SELECCIONA AL CONTACTO QUE RECIVIRA EL MSJ
+                wallPage.FBClSelectContact.Click();
+                //REDACTAR TEXTO DEL MSJ
+                wallPage.FBChatTextBox.SendKeys("Ejercicio de practica del curso de automation con selenium");
+                //ENVIAR EL MSJ
+                wallPage.FBSentMsgButton.Click();
+
+                isMessageSend = true;
+                //Assert.IsTrue(wallPage.FBMsgText.Displayed);
+                return isMessageSend;
+            }
+            catch (Exception e)
+            {
+                TestContext.Error.WriteLine("LoginUser test failed " + e.ToString());
+                return false;
+            }
+        }
+    }
+}
