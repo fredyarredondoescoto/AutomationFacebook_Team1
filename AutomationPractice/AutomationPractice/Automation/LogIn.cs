1dc776ed91f6b9e0363ffe8238a6d5f9343405b8
Alfredo Arredondo <fredyarredondoescoto@gmail.com>
2020-06-10 13:08:43 -0500

    Login Automated
    Adjunto la automatización del login para facebook

---
+++ AutomationPractice/AutomationPractice/Automation/LogIn.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
+using AutomationPractice.Locators;
+using OpenQA.Selenium.Chrome;
+
+namespace AutomationPractice.Automation
+{
+    class LogIn
+    {
+        private RemoteWebDriver driver;
+        private WebDriverWait wait;
+
+        LoginLocators loginPage;
+
+        public LogIn(RemoteWebDriver Driver, WebDriverWait Wait)
+        {
+            driver = Driver;
+            wait = Wait;
+        }
+
+        public bool LoginAccount(RemoteWebDriver driver)
+        {
+            var isLoginAccount = false;
+            try
+            {
+                loginPage = new LoginLocators(driver, wait);
+                loginPage.emailBox.Click();
+                loginPage.emailBox.SendKeys("selenfacepractica1@gmail.com");
+                loginPage.pswBox.Click();
+                loginPage.pswBox.SendKeys("pecescolorsalmon");
+                loginPage.loginButton.Click();
+
+                isLoginAccount = true;
+
+                return isLoginAccount;
+
+            }
+            catch (Exception e)
+            {
+                TestContext.Error.WriteLine("LoginAccount test failed" + e.ToString());
+                return false;
+            }
+        }
+    }
+}
---
+++ AutomationPractice/AutomationPractice/Locators/LoginLocators.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
+
+namespace AutomationPractice.Locators
+{
+    class LoginLocators
+    {
+        //SE MANDA LLAMAR EL DRIVER
+        private RemoteWebDriver driver;
+        private WebDriverWait wait;
+
+        public LoginLocators(RemoteWebDriver Driver, WebDriverWait Wait)
+        {
+            driver = Driver;
+            wait = Wait;
+        }
+
+        public IWebElement emailBox => driver.FindElement(By.CssSelector("input[name='email']"));
+        public IWebElement pswBox => driver.FindElement(By.CssSelector("input[name='pass']"));
+        public IWebElement loginButton => driver.FindElement(By.CssSelector("[type='submit']"));
+    }
+}
